2fead4bb826d4706885f13d8ee5626784bb37f38
deepeters <dennis95peters@gmail.com>
2022-04-05 07:10:01 +0300

    Use rest template to call an external microservice API

--- movie-catalogue-service/src/main/java/ke/springboot/resources/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/ke/springboot/resources/MovieCatalogueResource.java
@@ -1,12 +1,17 @@
 package ke.springboot.resources;
 
 import ke.springboot.models.CatalogueItem;
+import ke.springboot.models.Movie;
+import ke.springboot.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -16,9 +21,22 @@ public class MovieCatalogueResource {
     @RequestMapping("/{userId}")
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-                new CatalogueItem("Lord of the Rings", "A bunch of people taking a ring to a volcano", 5)
+        RestTemplate restTemplate = new RestTemplate();
+
+        //get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+                new Rating( "1234",  4),
+                new Rating( "5678",  5)
         );
+
+        return ratings.stream().map(rating -> {
+           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+           return new CatalogueItem(movie.getName(), "Description", rating.getRating());
+        }).collect(Collectors.toList());
+
+        //for each movie ID, call movie info service and get details
+
+        //put them all together
 
     }
 

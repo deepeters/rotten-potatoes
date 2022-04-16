384a039d714b79173ef7c76f2d528fb9d5f94874
deepeters <dennis95peters@gmail.com>
2022-04-16 17:36:02 +0300

    remove hard coded urls and allow for service discovery using eureka

--- movie-catalogue-service/src/main/java/ke/springboot/resources/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/ke/springboot/resources/MovieCatalogueResource.java
@@ -35,12 +35,12 @@ public class MovieCatalogueResource {
 
 
         //get all rated movie IDs
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
 
             //for each movie ID, call movie info service and get details
-           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+           Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
 //            Movie movie = webClientBuilder.build()
 //                   .get()
